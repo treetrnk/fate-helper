3cdfa5dd7b4b5f67e16f15262aab34e16f2e5c36
Nathan Hare <nhare330@gmail.com>
2015-12-07 06:30:44 -0500

    Change permissions and begin work on js

--- js/fateHelper.js
+++ js/fateHelper.js
@@ -1,4 +1,23 @@
-angular.module('aspectApp', [])
-  .controller('aspectController', function() {
-    
+angular.module('fateHelper', [])
+  .controller('CardController', function() {
+    var card = this;
+    card.name = 'Name';
+    card.highConcept = 'High concept';
+    card.trouble = 'Trouble';
+    card.aspects = [
+      {type: '', text: ''}];
+    card.skills = [
+      {text: '', level: ''}];
+    card.stunts = [];
+    card.stress = [
+      {level: 1, type: 'physical'},
+      {level: 2, type: 'physical'},
+      {level: 3, type: 'physical'},
+      {level: 1, type: 'mental'},
+      {level: 2, type: 'mental'},
+      {level: 3, type: 'mental'}];
+    card.consequences = [
+      {level: 2, text: ''}, 
+      {level: 4, text: ''}, 
+      {level: 6, text: ''}];
   });
